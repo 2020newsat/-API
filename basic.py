784db33b4473d526612fac3b6834ba65c1cc9d2a
2020newsat <124790118+2020newsat@users.noreply.github.com>
2023-02-20 19:59:04 +0900

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,78 +1,81 @@
-import win32com.client
-import pythoncom
+from pandas import DataFrame
 import datetime
-from pandas import Series, DataFrame
 
-import sys
-
-sys.path.append("./function")
-
-from tr import *
+from session import *
+from query import *
 from etc import *
+from wait import *
+from tr import *
 
-# ----------------------------------------------------------------------------
-# parameter
-# ----------------------------------------------------------------------------
-
-opymd = 'W3    '
-# 월물은 '202303' / 위클리는 'W1    ' (총 6글자, 만기주 위클 없으니 'W1    ' 다음은  'W3    ')
-
-# id, passwd, acn, pwd
-maxQty = 2
-
-# ----------------------------------------------------------------------------
-# login
-# ----------------------------------------------------------------------------
-login(id, passwd, cert_passwd)
+# parameter id, passwd, cert, acn etc
+id
+passwd
+cert =
+acn = # 계좌번호
+pwd = '0000'  # 계좌비번
+qty = 2
+Opym = 'W4    '
+OpGubun = 'W'
 today = datetime.datetime.today()
 ymd = today.strftime("%Y%m")
+w = today.weekday()
 
+# 로그인 (Session)
+print("login requst")
+session = XASession()
+session.login(id, passwd, cert, block=True)
+print("login done")
+acc = session.get_account_list(0)
+print(acc)
+
+# ----------------------------------------------------------------------------
+# T0441 계좌잔고확인
+# ----------------------------------------------------------------------------
+balance = DataFrame()
+balance = ClearBalance(acn, pwd)
+print(balance)
+
+# ----------------------------------------------------------------------------
+# 잔고청산
+# ----------------------------------------------------------------------------
 order = DataFrame()
+for i in range(len(balance)):
+    code = balance.iloc[i, 0]
+    q = int(balance.iloc[i, 2])
+    if balance.iloc[i, 1] == '매수':
+        bns = 's'
+    if balance.iloc[i, 1] == '매도':
+        bns = 'b'
 
-balance = T0441(acn, pwd)
+    order = SetOrder(code, q, bns, order)
 
-if len(balance.index) > 0:
-    balance['qty'] = balance['qty'].astype(int)
-    order = ClearBalance(balance, maxQty, order)
-    order['qty'] = order['qty'].astype(int)
-    print('clear order:', order)
+# 요일확인, 동시호가 대기
+w = today.weekday()
+WaitUntil()
+# Option Price Table
+# strike     cCode   call     pCode    put  LenCP   LenUD
 
-op = T2301(opymd)
-kp200 = T1511(101)
-atm = atm(kp200, op)
+opt = DataFrame()
+opt = op(Opym, OpGubun)
+print(opt)
 
-# ----------------------------------------------------------------------------
+# calculate ATM
+minUD = opt['LenUD'].min()
+print(minUD)
+Uindex = opt.loc[opt['LenUD'] == minUD].index[0]
+Ulen = opt.iloc[Uindex, 5]
+Dlen = opt.iloc[Uindex + 1, 5]
+Mlen = min(Ulen, Dlen)
+
+if minUD <= Mlen:
+    c1 = opt.iloc[Uindex, 0]
+    p1 = c1 - 2.5
+else:
+    c1 = opt.loc[opt['LenCP'] == Mlen].strike.iloc[0]+2.5
+    p1 = c1 - 5
+print(c1, p1)
+
 # Strategy
-# ----------------------------------------------------------------------------
 
-
-
-# ----------------------------------------------------------------------------
-# RunOrder: tmCode - bid ask - enter
-# ----------------------------------------------------------------------------
-print("order ready: ", order)
-
-for row in range(len(order)):
-    code = order.iloc[row, 0]
-    qty = order.iloc[row, 1]
-    strike = CodeToStrike(code)
-    BidAsk = T2105(code)  # bid/ask 매수/매도 3호가까지 확인
-
-    if qty > 0:
-        bns = '2'
-        price = BidAsk['ask3']
-
-    if qty < 0:
-        bns = '1'
-        qty = - qty
-        price = BidAsk['bid3']
-
-    if qty != 0:
-        for OrdCount in range(qty):
-            print("OrdCount is", OrdCount, code, bns, price)
-            OrdNo = CFOAT00100(code, bns, '00', price, 1, acn, passwd)
-            #print(OrdNo)
-
-# CFOAT00100(code, BnS, kind, price, qty) - return: OrdNo
-# BnS '1' 매도 '2' 매수
-# kind '00' 지정가 / '03' 시장가
+# 세팅된 주문 실행
+runOrder(order, acn, passwd)
